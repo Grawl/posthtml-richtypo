a88eeff9f8ac504c34bf11a5411ccc73d4bc74fb
Даниил Пронин <mail@grawl.ru>
2020-06-03 16:20:29 +1000

    change indentation

--- test/test.js
+++ test/test.js
@@ -8,14 +8,14 @@ const posthtml = require('posthtml')
 const fixtures = path.join(__dirname, 'fixtures')
 
 test('basic', (t) => {
-    return compare(t, 'basic')
+  return compare(t, 'basic')
 })
 
 function compare (t, name) {
-    const html = readFileSync(path.join(fixtures, `${name}.html`), 'utf8')
-    const expected = readFileSync(path.join(fixtures, `${name}.expected.html`), 'utf8')
+  const html = readFileSync(path.join(fixtures, `${name}.html`), 'utf8')
+  const expected = readFileSync(path.join(fixtures, `${name}.expected.html`), 'utf8')
 
-    return posthtml([plugin()])
-        .process(html)
-        .then((res) => t.truthy(res.html === expected))
+  return posthtml([plugin()])
+    .process(html)
+    .then((res) => t.truthy(res.html === expected))
 }
